5cf4186d123992da78bfa72c76587bbe3fad0b13
LovepreetSingh-09 <54183912+LovepreetSingh-09@users.noreply.github.com>
2019-10-24 14:55:32 +0530

    Update Feed_Forward_NN.py

--- Feed_Forward_NN.py
+++ Feed_Forward_NN.py
@@ -4,13 +4,14 @@ Created on Wed Oct  9 20:56:16 2019
 
 @author: user
 """
-
+import torch
 import sys
 import os
 import struct
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 #from keras.datasets import mnist
 #
 #(x_train,y_train),(x_test,y_test)=mnist.load_data()
